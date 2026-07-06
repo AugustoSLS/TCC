362a9240a977b17baa333985c92d52203f9d145b
Augusto <88404484+AugustoSLS@users.noreply.github.com>
2026-07-06 13:02:20 -0300

    estrutura documento

--- tcc_unip/src/pages/Clientes.jsx
+++ tcc_unip/src/pages/Clientes.jsx
@@ -4,11 +4,12 @@ import '../styles/client.css'
 //
 
 export default function Clients() {
-
+    
     return (
     <section className="cBar">
         <div className="sBar">
             <button className="vCard"></button>  
+            <p>Valor {Number}</p>
             <button className="vHorz"></button>
         </div>
     </section>
